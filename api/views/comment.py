d01a3a2c5bb0561f190406585f0b36ac25beb190
grigorevskiy <grigorevskiiy@gmail.com>
2018-09-04 20:16:13 +0300

    Refactoring

--- api/views/comment.py
+++ api/views/comment.py
@@ -4,32 +4,25 @@ from rest_framework.permissions import IsAuthenticated
 from api.serializers.comment import CommentSerializer
 from api.models import Comment
 from ..permissions import IsOwner
+from rest_framework import permissions
 
 
-class JourneyCommentsCreateAPIView(CreateAPIView):
-    permission_classes = (IsAuthenticated,)
+class JourneyCommentsView(ListCreateAPIView):
+    """Get Comments for Event or Create new Comment for this Event"""
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = CommentSerializer
 
-    # def get_queryset(self):
-    #     return Comment.objects.filter(journey_id=self.kwargs.get('pk', 0))
+    def get_queryset(self):
+        return Comment.objects.filter(journey_id=self.kwargs.get('pk', 0)).select_related('user')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user, journey_id=self.kwargs.get('pk', 0))
 
 
-class JourneyCommentsListAPIView(ListAPIView):
-    authentication_classes = [SessionAuthentication]
-
-    serializer_class = CommentSerializer
-
-    def get_queryset(self):
-        return Comment.objects.filter(journey_id=self.kwargs.get('pk', 0))
-
-
-class JourneyCommentsDetailView(RetrieveUpdateDestroyAPIView):
+class JourneyCommentsDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, IsOwner)
     serializer_class = CommentSerializer
     lookup_url_kwarg = 'com_pk'
 
     def get_queryset(self):
-        return Comment.objects.filter(journey_id=self.kwargs.get('pk', 0))
+        return Comment.objects.filter(event_id=self.kwargs.get('pk', 0))
