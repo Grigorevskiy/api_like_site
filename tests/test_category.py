81b1104570915f31ae4a89e2f505669c3d3bcff0
grigorevskiy <grigorevskiiy@gmail.com>
2018-09-07 12:15:04 +0300

    Added test for category

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,4 +1,6 @@
 
+import json
+
 from rest_framework.test import APITestCase
 from django.test import Client
 from api.models import Category
@@ -10,23 +12,14 @@ class TestCategoryApi(APITestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create()
-        self.category = Category.objects.create(name="Test Category")
+        self.category = Category.objects.create(name="Test Category",
+                                                description="Des")
 
     def test_get_categories(self):
         response = self.client.get('/category/')
         self.assertEqual(200, response.status_code)
 
-    def test_get_categories_details(self):
-        user = User.objects.create_superuser(
-            email='test@mail.ru',
-            username='test',
-            password='test',
-        )
-        self.client.force_login(user)
-        response = self.client.get('/category/{}/'.format(self.category.pk), data={'user': user.id})
-        self.assertEqual(200, response.status_code)
-
-    def test_get_categories_details_as_not_admin(self):
+    def test_get_category_details(self):
         response = self.client.get('/category/{}/'.format(self.category.pk))
         self.assertEqual(200, response.status_code)
 
@@ -35,8 +28,6 @@ class TestCategoryApi(APITestCase):
         self.client.force_login(user)
         response = self.client.post('/category/', data={'name': 'Title',
                                                                 'description': 'Description'})
-
-        # assert a 201 status code was returned
         self.assertEqual(201, response.status_code)
 
     def test_create_category_as_not_admin(self):
@@ -46,3 +37,32 @@ class TestCategoryApi(APITestCase):
 
         # assert a 201 status code was returned
         self.assertEqual(403, response.status_code)
+
+    def test_update_category_as_admin(self):
+        user = User.objects.create_superuser(email='test@mail.ru', username='test', password='test')
+        self.client.force_login(user)
+
+        update_category = self.client.put('/category/{}/'.format(self.category.pk),
+                                          data=json.dumps({'name': 'new name1',
+                                                           'description': 'new des'}), content_type='application/json')
+        self.assertEqual(200, update_category.status_code)
+
+    def test_update_category_as_not_admin(self):
+        update_category = self.client.put('/category/{}/'.format(self.category.pk),
+                                          data=json.dumps({'name': 'new name1',
+                                                           'description': 'new des'}), content_type='application/json')
+
+        self.assertEqual(401, update_category.status_code)
+
+    def test_delete_category(self):
+        self.test_create_category_as_admin()
+
+        delete_category = self.client.delete('/category/{}/'.format(self.category.pk))
+        self.assertEqual(204, delete_category.status_code)
+
+        delete_category = self.client.delete('/category/{}/'.format(self.category.pk))
+        self.assertEqual(404, delete_category.status_code)
+
+    def test_delete_category_as_not_admin(self):
+        delete_category = self.client.delete('/category/{}/'.format(self.category.pk))
+        self.assertEqual(401, delete_category.status_code)
