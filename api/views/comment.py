8fc45b5d506425784ba98ffae27cb3fcd5b1bddc
grigorevskiy <grigorevskiiy@gmail.com>
2018-09-25 15:51:04 +0300

    Added caching likers if < 10

--- api/views/comment.py
+++ api/views/comment.py
@@ -6,7 +6,6 @@ from api.serializers.comment import CommentSerializer
 from api.models import Comment
 from ..permissions import IsOwner
 from rest_framework import permissions, status, viewsets
-
 # from django_redis import cache
 from django.core.cache import cache
 
@@ -20,38 +19,14 @@ class JourneyCommentsViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
             serializer.save(user=self.request.user)
 
-    @action(methods=['GET', 'DELETE'], detail=True)
-    def like(self, request, pk, *args, **kwargs):
-        comment = get_object_or_404(self.get_queryset().filter(pk=pk))
-
-        if request.method == 'GET':
-            if request.user.id not in comment.liked_by:
-                comment.liked_by.append(request.user.id)
-                comment.likes += 1
-        else:
-            if request.user.id in comment.liked_by:
-                comment.liked_by.remove(request.user.id)
-                comment.likes -= 1
-
-        comment.save()
-        return Response(status=status.HTTP_200_OK)
-
-    # ACTION WITH CHACHE
     # @action(methods=['GET', 'DELETE'], detail=True)
     # def like(self, request, pk, *args, **kwargs):
     #     comment = get_object_or_404(self.get_queryset().filter(pk=pk))
-    #     cached_likers = cache
     #
     #     if request.method == 'GET':
     #         if request.user.id not in comment.liked_by:
-    #             if cached_likers < 2:
-    #                 likers_to_cache = []
-    #                 likers_to_cache.append(request.user.id)
-    #                 cached_likers.set("likers", likers_to_cache)
-    #                 comment.likes += 1
-    #             else:
-    #                 comment.liked_by.append(request.user.id)
-    #                 comment.likes += 1
+    #             comment.liked_by.append(request.user.id)
+    #             comment.likes += 1
     #     else:
     #         if request.user.id in comment.liked_by:
     #             comment.liked_by.remove(request.user.id)
@@ -60,28 +35,43 @@ class JourneyCommentsViewSet(viewsets.ModelViewSet):
     #     comment.save()
     #     return Response(status=status.HTTP_200_OK)
 
+    @action(methods=['GET', 'DELETE'], detail=True)
+    def like(self, request, pk, *args, **kwargs):
+        comment = get_object_or_404(self.get_queryset().filter(pk=pk))
 
+        cached_likers = cache.get('likers{}'.format(comment.pk))
 
-    # @action(methods=['post'], detail=False, url_path='(?P<pk>[0-9]+)/like')
-    # def add_like(self, request, **kwargs):
-    #
-    #     comment = get_object_or_404(Comment, pk=kwargs.get('pk', 0))
-    #
-    #     if request.user.id not in comment.liked_by:
-    #         comment.liked_by.append(request.user.id)
-    #         comment.likes += 1
-    #         comment.save()
-    #
-    #     return Response(status=status.HTTP_200_OK)
-    #
-    # @action(methods=['delete'], detail=False, url_path='(?P<pk>[0-9]+)/unlike')
-    # def delete_like(self, request, **kwargs):
-    #
-    #     comment = get_object_or_404(Comment, pk=kwargs.get('pk', 0))
-    #
-    #     if request.user.id in comment.liked_by:
-    #         comment.liked_by.remove(request.user.id)
-    #         comment.likes -= 1
-    #         comment.save()
-    #
-    #     return Response(status=status.HTTP_200_OK)
+        if request.method == 'GET':
+            if request.user.id not in comment.liked_by:
+                if cached_likers is None:
+                    cached_likers = [request.user.id]
+                    cache.set('likers{}'.format(comment.pk), cached_likers)
+                    comment.likes += 1
+                    comment.save()
+
+                elif request.user.id not in cached_likers:
+                    if len(cache.get('likers{}'.format(comment.pk))) < 10:
+                        cached_likers.append(request.user.id)
+                        cache.set('likers{}'.format(comment.pk), cached_likers)
+                        comment.likes += 1
+                        comment.save()
+                    else:
+                        comment.likes += 1
+                        for i in cached_likers:
+                            comment.liked_by.append(i)
+                            comment.save()
+
+                        cache.delete('likers{}'.format(comment.pk))
+        else:
+            if request.user.id in comment.liked_by:
+                comment.liked_by.remove(request.user.id)
+                comment.likes -= 1
+                comment.save()
+
+            elif request.user.id in cached_likers:
+                cached_likers.remove(request.user.id)
+                cache.set('likers{}'.format(comment.pk), cached_likers)
+                comment.likes -= 1
+                comment.save()
+
+        return Response(status=status.HTTP_200_OK)
